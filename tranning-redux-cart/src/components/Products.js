65003bab49cbedabd979d5fba4c02403aab1b77a
BinhNguyen <nguyenvanbinh3790@gmail.com>
2021-06-14 14:16:12 +0700

    update code

--- tranning-redux-cart/src/components/Products.js
+++ tranning-redux-cart/src/components/Products.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import ProductItem from './ProductItem';
 
 class Products extends Component {
   render() {
-    var { products } = this.props;
     return (
       <section className="section">
         <h1 className="section-heading">Danh Sách Sản Phẩm</h1>
