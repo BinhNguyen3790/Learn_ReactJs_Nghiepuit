c942bdd5bc6af18004f0e3a0f6baf0bb79807092
BinhNguyen <nguyenvanbinh3790@gmail.com>
2021-06-18 09:16:58 +0700

    initial project

---
+++ lesson18-redux-saga/src/App.js
@@ -0,0 +1,12 @@
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello redux saga</h1>
+      <Button variant="contained" color="primary">Hello</Button>
+    </div>
+  );
+}
+
+export default App;
