f6bc599f1f0c141036a11eb3bd0e3de779ff7372
BinhNguyen <nguyenvanbinh3790@gmail.com>
2021-06-22 09:24:29 +0700

    create template

--- lesson18-redux-saga/src/containers/Taskboard/index.js
+++ lesson18-redux-saga/src/containers/Taskboard/index.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/styles';
 import styles from './styles';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import Grid from '@material-ui/core/Grid';
+import { STATUSES } from '../../constants';
+
 
 class Taskboard extends Component {
+  renderBoard() {
+    let xhtml = null;
+    xhtml = (
+      <Grid container spacing={2}>
+        {STATUSES.map((status, index) => {
+          return (
+            <Grid item md={4} xs={12} key={status.value}>{status.label}</Grid>
+          )
+        })}
+      </Grid>
+    );
+    return xhtml;
+  };
   render() {
     const { classes } = this.props;
     return (
-      <div>
-        <div className={classes.boxTask}>
-          <div className={classes.boxTask__item}><div className={classes.title}><h2>React Js</h2></div></div>
-          <div className={classes.boxTask__item}><div className={classes.title}><h2>Angular Js</h2></div></div>
-          <div className={classes.boxTask__item}><div className={classes.title}><h2>Vue Js</h2></div></div>
-        </div>
+      <div className={classes.taskBoard}>
+        <Button variant="contained" color="primary"><AddIcon /> Add Job</Button>
+        {this.renderBoard()}
       </div>
     )
   }
