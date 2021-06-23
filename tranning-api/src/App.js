e5e0b38c228c7dfbe26da6fd07161e340a533079
BinhNguyen <nguyenvanbinh3790@gmail.com>
2021-06-23 10:43:37 +0700

    create router

--- tranning-api/src/App.js
+++ tranning-api/src/App.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
 import Menu from './components/Menu/Menu';
 import ProductList from './components/ProductList/ProductList';
+import routes from './routes'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Menu />
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
+      <Router>
+        <div>
+          <Menu />
+          <div className="container">
+            <div className="row">
+              {/* <div className="col-12">
               <button type="button" className="btn btn-primary mb-2">Add Product</button>
               <ProductList />
+            </div> */}
+              {this.showContentMenu(routes)}
             </div>
           </div>
         </div>
-      </div>
+      </Router>
     )
+  };
+  showContentMenu = (routes) => {
+    var result = null;
+    if (routes.length > 0) {
+      result = routes.map((route, index) => {
+        return (
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={route.main}
+          />
+        )
+      })
+    }
+    return <Switch>{result}</Switch>;
   }
 }
 export default App;
