7c9500dbd86ede8825bfe1ce147ebbd834780b47
BinhNguyen <nguyenvanbinh3790@gmail.com>
2021-07-29 20:57:30 +0700

    update delete

--- tranning-api/src/pages/ProductListPage/ProductListPage.js
+++ tranning-api/src/pages/ProductListPage/ProductListPage.js
@@ -2,45 +2,16 @@ import React, { Component } from 'react';
 import ProductList from '../../components/ProductList/ProductList';
 import ProductItem from '../../components/ProductItem/ProductItem';
 import { connect } from 'react-redux';
-import callApi from '../../ultils/apiCaller';
 import { Link } from 'react-router-dom';
-import { actFetchProductsRequest } from './../../actions/index';
+import { actFetchProductsRequest, actDeleteProductRequest } from './../../actions/index';
 class ProductListPage extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: []
-    }
-  };
 
   componentDidMount() {
     this.props.fetchAllProducts();
   };
 
   onDelete = (id) => {
-    var { products } = this.state;
-    callApi(`products/${id}`, "DELETE", null).then(res => {
-      if (res.status === 200) {
-        var index = this.findIndex(products, id);
-        if (index !== -1) {
-          products.splice(index, 1);
-          this.setState({
-            products: products
-          })
-        }
-      }
-    })
-  }
-
-  findIndex = (products, id) => {
-    var result = -1;
-    products.forEach((product, index) => {
-      if (product.id === id) {
-        result = index;
-      }
-    })
-    return result;
+    this.props.onDeleteProduct(id);
   }
 
   render() {
@@ -76,7 +47,10 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch, props) => {
   return {
     fetchAllProducts: () => {
-      dispatch(actFetchProductsRequest())
+      dispatch(actFetchProductsRequest());
+    },
+    onDeleteProduct: (id) => {
+      dispatch(actDeleteProductRequest(id));
     }
   }
 }
